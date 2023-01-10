c69a3cddc04b6150402b107191f0e057beb26d3e
Caroline Crandell <cecrandell19@gmail.com>
2023-01-10 15:24:03 -0500

    Navbar complete

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDJvfOue_03LjyuIcxxfDYIg42_kFE2Mmc",
+    authDomain: "netflix-fd2ce.firebaseapp.com",
+    projectId: "netflix-fd2ce",
+    storageBucket: "netflix-fd2ce.appspot.com",
+    messagingSenderId: "929281323193",
+    appId: "1:929281323193:web:f30788ed7af3cc5773ab68"
+  };
