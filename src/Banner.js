e88c813e935f44c5d290b2f1762b46899e71492e
Caroline Crandell <cecrandell19@gmail.com>
2023-01-10 21:04:37 -0500

    Row component complete

--- src/Banner.js
+++ src/Banner.js
@@ -32,16 +32,15 @@ function Banner() {
       }}
     >
       <div className="banner__contents">
-        <h1 className="banner_title">Movie</h1>
+        <h1 className="banner__title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
         <div className="banner__buttons">
           <button className="banner__button">Play</button>
           <button className="banner__button">My List</button>
         </div>
         <h1 className="banner__description">
-          {truncate(
-            "This is a test description This is a test description This is a test description This is a test description This is a test description This is a test description This is a test description This is a test description This is a test description This is a test description This is a test description This is a test description This is a test description This is a test description",
-            150
-          )}
+          {truncate(movie?.overview, 150)}
         </h1>
       </div>
       <div className="banner--fadeBottom" />
