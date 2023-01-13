2f4b6aea1392fbb826c94e1864776e55d9b5c9a2
Caroline Crandell <cecrandell19@gmail.com>
2023-01-12 19:58:15 -0500

    Initialize Firebase authentication

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,17 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
-    apiKey: "AIzaSyDJvfOue_03LjyuIcxxfDYIg42_kFE2Mmc",
-    authDomain: "netflix-fd2ce.firebaseapp.com",
-    projectId: "netflix-fd2ce",
-    storageBucket: "netflix-fd2ce.appspot.com",
-    messagingSenderId: "929281323193",
-    appId: "1:929281323193:web:f30788ed7af3cc5773ab68"
-  };
+  apiKey: "AIzaSyDJvfOue_03LjyuIcxxfDYIg42_kFE2Mmc",
+  authDomain: "netflix-fd2ce.firebaseapp.com",
+  projectId: "netflix-fd2ce",
+  storageBucket: "netflix-fd2ce.appspot.com",
+  messagingSenderId: "929281323193",
+  appId: "1:929281323193:web:f30788ed7af3cc5773ab68",
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { auth };
+export default db;
